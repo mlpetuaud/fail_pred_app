1d03f6cf13e2019f23b31ebbf8f986ef35f52851
mlpetuaud <mlmaurisset@yahoo.fr>
2022-07-22 16:33:16 +0200

    heroku

--- db_settings.py
+++ db_settings.py
@@ -6,11 +6,12 @@ dotenv.load_dotenv(dotenv_path)
 
 # DB CREDENTIALS (local settings)
 #DB_PASSWORD= os.environ.get("DB_PASSWORD")
-DB_NAME=os.environ.get("DB_NAME")
-DB_USERNAME=os.environ.get("DB_USERNAME")
-DB_ADDRESS=os.environ.get("DB_ADDRESS")
-DB_PORT=os.environ.get("DB_PORT")
+#DB_NAME=os.environ.get("DB_NAME")
+#DB_USERNAME=os.environ.get("DB_USERNAME")
+#DB_ADDRESS=os.environ.get("DB_ADDRESS")
+#DB_PORT=os.environ.get("DB_PORT")
 
 # Scheme: "postgresql://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-DATABASE_URI = f"postgresql://{DB_USERNAME}@{DB_ADDRESS}:{DB_PORT}/{DB_NAME}"
+#DATABASE_URI = f"postgresql://{DB_USERNAME}@{DB_ADDRESS}:{DB_PORT}/{DB_NAME}"
+DATABASE_URI = os.environ.get("DATABASE_URL")
 print(DATABASE_URI)
